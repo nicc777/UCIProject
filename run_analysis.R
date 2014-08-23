eb21c0845a13c7754b49a3afe3dbd4c71a57775f
Nico Coetzee <nicc777@gmail.com>
2014-08-23 14:42:53 +0200

    All 5 points done. Final testing before we do the final refactoring to remove all debug code.

--- run_analysis.R
+++ run_analysis.R
@@ -6,61 +6,177 @@
 #   testdit  - Location to the directory containing the test data set
 #   outfile  - The file name of the tidy data set
 
-run_analysis <- function(traindir="train", testdir="test", outfile="out.txt",verbose=FALSE){
-    # Preperation: Load the data sets, each into a seperate data frame
+run_analysis <- function(traindir="train", testdir="test", outfile="out.txt"){
+
+    # Load libraries on which we depend.
+    library(plyr)
+    library(reshape2)
+
+    ########################################
+    # Preperation Phase
+    ########################################
 
     # Load Activity Labels
     activityLabels <- load_singlecolumn_data_as_vector("activity_labels.txt",columnNr=2)
-    if( verbose == TRUE ) message(paste("activityLabels: ", activityLabels,"\n"))
 
-    # Load the features.txt as column labels
+    # Load the features.txt as column labels and prepare the labels for the
+    # TIDY set
     dataColumnLabels <- load_singlecolumn_data_as_vector("features.txt",columnNr=2)
-    if( verbose == TRUE ) message(paste("dataColumnLabels: ", head(dataColumnLabels),"\n"))
+    tidyDataLabels <- c(
+        grep_labal_names(dataColumnLabels,".*mean.*"),
+        grep_labal_names(dataColumnLabels,".*std.*"),
+        "ActivityLabel",
+        "Subject")
 
     # Load data sets (data frame)
     trainDF <- load_data(paste(traindir,"X_train.txt",sep=.Platform$file.sep))
     names(trainDF) <- dataColumnLabels
-    if( verbose == TRUE ) message(paste("trainDF: nr of rows: ", nrow(trainDF)))
     testDF <- load_data(paste(testdir,"X_test.txt",sep=.Platform$file.sep))
     names(testDF) <- dataColumnLabels
-    if( verbose == TRUE ) message(paste("testDF: nr of rows: ", nrow(testDF)))
 
     # Load the subject data (vector - object count corresponds to data frame rows)
     trainSubjects <- load_singlecolumn_data_as_vector(paste(traindir,"subject_train.txt",sep=.Platform$file.sep))
-    if( verbose == TRUE ) message(paste("trainSubjects: nr of subjects: ", length(trainSubjects)))
     testSubjects <- load_singlecolumn_data_as_vector(paste(testdir,"subject_test.txt",sep=.Platform$file.sep))
-    if( verbose == TRUE ) message(paste("testSubjects: nr of subjects: ", length(testSubjects)))
 
     # Load activity data (vector - object count corresponds to data frame rows)
     trainActivities <- load_singlecolumn_data_as_vector(paste(traindir,"y_train.txt",sep=.Platform$file.sep))
     trainActivities <- remap_activities(trainActivities,activityLabels)
-    trainDF[,"train.ActivityLabel"] <- trainActivities
-    if( verbose == TRUE ) message(paste("trainActivities: nr of subjects: ", length(trainActivities)))
+    trainDF[,"ActivityLabel"] <- trainActivities
     testActivities <- load_singlecolumn_data_as_vector(paste(testdir,"y_test.txt",sep=.Platform$file.sep))
     testActivities <- remap_activities(testActivities,activityLabels)
-    testDF[,"test.ActivityLabel"] <- testActivities
-    if( verbose == TRUE ) message(paste("testActivities: nr of subjects: ", length(testActivities)))
+    testDF[,"ActivityLabel"] <- testActivities
 
     # Add the Subjects to the data sets for training and test data sets
-    trainDF[,"train.Subject"] <- trainSubjects
-    testDF[,"test.Subject"] <- testSubjects
+    trainDF[,"Subject"] <- trainSubjects
+    testDF[,"Subject"] <- testSubjects
 
     #x <<- testDF      # > View(head(x[,c(500:563)]))      # IGNORE!!! Mark for delete
     #y <<- trainDF
 
+    ########################################
+    # Final Phase
+    ########################################
+
     # 1. Merges the training and the test sets to create one data set.
+    masterDF <- rbind(trainDF,testDF)
+
     # 2. Extracts only the measurements on the mean and standard deviation for
     #    each measurement.
+    tidyDF1 <- masterDF[,tidyDataLabels]
+
     # 3. Uses descriptive activity names to name the activities in the data set
+    # DONE EARLIER
+
     # 4. Appropriately labels the data set with descriptive variable names.
+    names(tidyDF1) <- gsub_batch(
+        names(tidyDF1),
+        c("^t","^f","std","\\.","\\(\\)"),
+        c("Time","Frequency","StandardDiv","",""))
+
+    # Used the following only to have a look at the data. Since we are not
+    # instructed to to anything else with this data frame, I am not writing it
+    # out to file or anything like that. Only using it as starting point for
+    # point 5
+    x <<- tidyDF1
+
     # 5. Creates a second, independent tidy data set with the average of each
     #    variable for each activity and each subject.
+
+    measureLabels <- as.vector(names(tidyDF1)[!(names(tidyDF1) %in% c("Subject", "ActivityLabel"))])
+
+    # Prepare data frame and column names
+    tidyDF2 <- prepare_tidy_dataframe(
+        sort(unique(as.vector(tidyDF1$Subject))),
+        activityLabels,
+        measureLabels)
+    names(tidyDF2) <- c(
+        "Subject",
+        "Activity",
+        measureLabels)
+
+    # Create a melted data frame for easy extraction and calculation of the
+    # avg. for measures grouped by Subject and Activity
+    tidyDF1Melted <- melt(
+        tidyDF1,
+        id.vars=c("Subject", "ActivityLabel"),
+        measure.vars=names(tidyDF1)[!(names(tidyDF1) %in% c("Subject", "ActivityLabel"))],
+        variable.name = "varName",
+        value.name = "measure")
+    # Add a group label for easier identification of Subject, Activity and
+    # Measure to the melted data frame
+    tidyDF1Melted$GroupLabel <- paste(
+        tidyDF1Melted$Subject,
+        tidyDF1Melted$ActivityLabel,
+        tidyDF1Melted$varName, sep=".")
+    # Extract the group labels and start calculating the average for each
+    # measure per Subject and Activity. Update tidyDF2 with the result.
+    groupLabels <- unique(as.vector(tidyDF1Melted[,"GroupLabel"]))
+    for(group in groupLabels){
+        elements <- unlist(strsplit(group,"\\."))
+        subjRef = elements[1]
+        actvRef = elements[2]
+        measRef = elements[3]
+        meanVal <- mean(tidyDF1Melted[tidyDF1Melted$GroupLabel == group,"measure"])
+        tidyDF2[(tidyDF2$Subject==subjRef & tidyDF2$Activity == actvRef),c(measRef)] <- meanVal
+    }
+
+    y <<- tidyDF1Melted
+    xx <<- tidyDF2
+
+    # Write the data to file - text file with TAB seperated values.
+    write.table(tidyDF2,outfile, sep = "\t", col.names=TRUE, row.names=FALSE)
+
+}
+
+# Preperation of the final tidy data frame. Pre-populate with each subject and
+# activity and set the default value for the average of each measure to NA.
+# Input Arguments:
+#   subjects   - Vector containing Subjects
+#   activities - Activity Labels
+#   measures   - Measurement Labels
+# Output:
+#   Data frame prepared and populated with NA values for each average measure
+prepare_tidy_dataframe <- function(subjects, activities, measures){
+    subjectCol <- c()
+    activityCol <- c()
+    for( subjID in subjects){
+        #message(paste("Adding subject: ", subjID))
+        for(activityLabel in activities){
+            #message(paste("Adding activity: ", activityLabel))
+            subjectCol <- c(subjectCol, subjID)
+            activityCol <- c(activityCol, activityLabel)
+        }
+    }
+    tidyDF <- data.frame(subjectCol, activityCol)
+    for( measure in measures){
+        tidyDF <- cbind(tidyDF,NA)
+    }
+    tidyDF
+}
+
+
+# Batch replace labels, using the gsub function
+# Input Arghuments:
+#   labels   - Column names of a data frame
+#   regexV   - Vector containing regex to search for
+#   replaceV - Vector (same length as regexV) with the replacement values
+# Output:
+#   Updated labels
+gsub_batch <- function(labels,regexV,replaceV){
+    idx = 1
+    for(rx in regexV){
+        labels <- gsub(rx, replaceV[idx], labels)
+        idx <- idx + 1
+    }
+    labels
 }
 
 # Remapping of a vector - replace activity index numbers with a labels
 # Input Arguments
 #   x - A vector containing activity values, i.e. c(1,1,2,2,3)
 #   labels - A vector containing labels, i.e. c("col1","col2")
+# Output:
+#   A vector with labels
 remap_activities <- function(x, labels){
     idx = 1
     for( lbl in labels){
@@ -73,6 +189,8 @@ remap_activities <- function(x, labels){
 # Loading the data set into a data frame
 # Input Arguments:
 #   srcfile - The file to load
+# Output:
+#   A data frame
 load_data <- function(srcfile){
     df <- emptydataframe()
     result <- tryCatch({
@@ -88,12 +206,26 @@ load_data <- function(srcfile){
 # Input Arguments:
 #   filename - The file to load as a data frame, wrapping load_data()
 #   columnNr - The column number to extract as a vector (default=1)
+# Output:
+#   A vector with the extracted values
 load_singlecolumn_data_as_vector <- function(filename, columnNr=1){
     df <- load_data(filename)
     as.vector(df[,columnNr])
 }
 
+# Search for only certain label names given a regular expression
+# Input Arguments:
+#   labels   - The vector containing all the labels
+#   regexstr - The string to match in each label
+# Output:
+#   Vector containing the matching strings
+grep_labal_names <- function(labels,regexstr){
+    c(grep(regexstr, x=labels, ignore.case=TRUE, value=TRUE))
+}
+
 # Function to create an empty data frame
+# Output:
+#   An empty dataframe
 emptydataframe <- function(){
     data.frame(Date=as.Date(character()),
                File=character(),
